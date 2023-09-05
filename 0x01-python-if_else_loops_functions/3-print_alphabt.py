7d42c1568dac60ee54e638fcef2a1085fe1bd1b2
NateMachoka <natemachoka@gmail.com>
2023-09-05 11:06:26 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for ascii_value in range(97, 123):
-    if(chr(ascii_value)) not in 'qe':
-       print("{}".format(chr(ascii_value)), end ='')
+    if chr(ascii_value) not in 'qe':
+        print("{}".format(chr(ascii_value)), end='')
