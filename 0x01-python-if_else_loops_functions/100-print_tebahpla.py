fdc4a4da3a32f067c422e7d694d5329834b2894d
NateMachoka <natemachoka@gmail.com>
2023-09-05 16:15:00 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in reversed(range(97, 123)):
     if i % 2 == 1:
-        print(f"{chr(i - 32)}", end='')
+        print("{}".format(chr(i - 32)), end='')
     else:
-        print(f"{chr(i)}", end='')
+        print("{}".format(chr(i)), end='')
