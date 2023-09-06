7a68dce4f201cbf661a66344f461a915bd25d76a
NateMachoka <natemachoka@gmail.com>
2023-09-06 02:58:13 -0700

    100-write.py

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 import sys
-message = "and that piece of art is useful - Dora Korpar, 2015-10-19"
-sys.stderr.write(message)
-exit(1)
+def main():
+    message = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+    sys.stderr.write(message)
+    sys.exit(1)
+
+if __name__ == "__main__":
+    main()
