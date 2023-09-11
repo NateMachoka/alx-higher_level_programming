c4fb5e2815266418d5414969428077e3c90224cf
NateMachoka <natemachoka@gmail.com>
2023-09-11 13:19:46 -0700

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+def divisible_by_2(my_list=[]):
+    # Create an empty list to store the True/False values
+    result = []
+
+    # Iterate through the elements in the input list
+    for num in my_list:
+        is_divisible = num % 2 == 0
+
+        # Append True or False to the result list
+        result.append(is_divisible)
+    return result
