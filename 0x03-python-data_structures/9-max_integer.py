4610b3f99e74ba7fd281952e7c7731163f5ced41
NateMachoka <natemachoka@gmail.com>
2023-09-11 13:11:34 -0700

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if not my_list:  # Check if the list is empty
+        return None
+
+    max_value = my_list[0]  # Initialize max_value with the first element
+
+    for num in my_list:
+        if num > max_value:
+            max_value = num  # Update max_value if a larger number is found
+
+    return max_value
