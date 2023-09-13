8da0c96a74ab910923984523692ee975b8a55b31
NateMachoka <natemachoka@gmail.com>
2023-09-13 13:13:17 -0700

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman_dict = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+
+    result = 0
+    prev_value = 0
+
+    for symbol in reversed(roman_string):
+        if symbol not in roman_dict:
+            return 0  # Invalid Roman numeral
+
+        value = roman_dict[symbol]
+
+        if value < prev_value:
+            result -= value
+        else:
+            result += value
+
+        prev_value = value
+
+    return result
