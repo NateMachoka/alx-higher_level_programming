137a45ee2ae710630f47c4dcd798229c821d6b1d
NateMachoka <natemachoka@gmail.com>
2023-09-13 12:04:58 -0700

    2-uniq_add.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+def uniq_add(my_list=[]):
+    unique_integers = set()
+
+    sum_of_unique_integers = 0
+
+    for num in my_list:
+        if num not in unique_integers:
+            unique_integers.add(num)
+            sum_of_unique_integers += num
+
+    return sum_of_unique_integers
