5965d308ff0983f54f6e7213e6b58c32e94332d0
NateMachoka <natemachoka@gmail.com>
2023-09-26 09:25:58 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 
 class Square:
+    """
+    This class defines a square with a private size attribute.
+    """
+
     def __init__(self, size):
+        """
+        Initializes a new square instance with the given size.
+        :param size: The size of the square.
+        """
         self.__size = size
 
     def area(self):
+        """
+        Calculates and returns the area of the square.
+        :return: The area of the square.
+        """
         return self.__size * self.__size
