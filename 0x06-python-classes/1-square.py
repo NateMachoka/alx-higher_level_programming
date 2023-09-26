fffbeb06c310a4b0a105cac1a729068b4b19d1b9
NateMachoka <natemachoka@gmail.com>
2023-09-26 09:58:42 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,19 +2,39 @@
 
 class Square:
     """
-    This class defines a square with a private size attribute.
+    The `Square` class represents a square shape with a private size attribute.
+
+    Attributes:
+        __size (int): The size (side length) of the square.
+
+    Methods:
+        __init__(self, size):
+            Initializes a new square instance with the given size.
+
+        area(self):
+            Calculates and returns the area of the square.
+
+    Usage:
+        To create and use a `Square` object, first import the class into your
+Python script.
+        Then, create instances of the `Square` class and use its methods to
+work with squares.
     """
 
     def __init__(self, size):
         """
         Initializes a new square instance with the given size.
-        :param size: The size of the square.
+
+        Args:
+            size (int): The size (side length) of the square.
         """
         self.__size = size
 
     def area(self):
         """
         Calculates and returns the area of the square.
-        :return: The area of the square.
+
+        Returns:
+            int: The area of the square.
         """
         return self.__size * self.__size
