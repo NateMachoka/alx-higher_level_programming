6928ec2330f2d3d0739ae76143278e1dece6e35a
NateMachoka <natemachoka@gmail.com>
2023-09-26 10:08:09 -0700

    fixed 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 
-"""
+"""Has a square class
 This module contains a simple square class called Square.
 """
 
 
 class Square:
-    """
+    """A square shape class
     A basic class representing a square shape.
 
     Attributes:
