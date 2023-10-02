cc5b87552b328ce505dc5178cc4d907d56d7d057
NateMachoka <natemachoka@gmail.com>
2023-10-02 14:55:53 -0700

    fixed 9-rectangle.py

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+
+import sys
+
+
+
+def is_safe(board, row, col, N):
+    # Check if there is a queen in the same column
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+
+    # Check upper-left diagonal
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # Check upper-right diagonal
+    for i, j in zip(range(row, -1, -1), range(col, N)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+def solve_n_queens(N):
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [[0 for _ in range(N)] for _ in range(N)]
+
+    def solve(row):
+        if row == N:
+            for i in range(N):
+                for j in range(N):
+                    if board[i][j] == 1:
+                        print("Q", end="")
+                    else:
+                        print(".", end="")
+                print()
+            print()
+            return
+
+        for col in range(N):
+            if is_safe(board, row, col, N):
+                board[row][col] = 1
+                solve(row + 1)
+                board[row][col] = 0
+
+    solve(0)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: 
