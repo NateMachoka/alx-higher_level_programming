213eebaa5b5a374226e72c77b13bb8858e21230c
NateMachoka <natemachoka@gmail.com>
2023-10-09 16:06:44 -0700

    5-base_geometry.py

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Module containing an empty class BaseGeometry.
+"""
+
+class BaseGeometry:
+    """An empty class that can be used as a base for geometric classes.
+    """
+    pass
