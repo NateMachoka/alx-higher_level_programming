5515e62a388b3a4b850d75124aff5f61d7addb6e
NateMachoka <natemachoka@gmail.com>
2023-10-11 12:08:44 -0700

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,27 +6,24 @@ the number of characters written.
 """
 
 
+#!/usr/bin/python3
+
+"""Module for file manipulation"""
+
 def write_file(filename="", text=""):
     """
-    Write a string to a text file (UTF8) and return the number of characters
-written.
+    Write a string to a text file (UTF8) and return the number of characters written.
 
     Parameters:
-    filename (str): The name of the text file to write to. Default is an
-empty string.
-    text (str): The string to be written to the file. Default is empty string
+    filename (str): The name of the file to write to.
+    text (str): The text to be written to the file.
 
     Returns:
     int: The number of characters written to the file.
     """
-
+    # Open the file in write mode using the with statement
     with open(filename, 'w', encoding='utf-8') as file:
-            char_count = file.write(text)
-            return char_count
-    except PermissionError:
-        print(f"Permission denied: Cannot write to '{filename}'")
-        return -1
+        # Write the text to the file and get the number of characters written
+        num_chars_written = file.write(text)
 
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-        return 0
+    return num_chars_written
