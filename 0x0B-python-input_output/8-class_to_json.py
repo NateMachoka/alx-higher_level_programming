3d589d47d4f97db76ff70fc9cf0c171ebcc6b2d6
NateMachoka <natemachoka@gmail.com>
2023-10-11 13:56:34 -0700

    8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""Module for converting an object to a JSON-serializable dictionary"""
+
+def class_to_json(obj):
+    """
+    Convert an object to a dictionary description for JSON serialization.
+
+    Parameters:
+    obj (object): The object to be converted to a JSON-serializable dictionary.
+
+    Returns:
+    dict: A dictionary representation of the object with serializable attributes.
+    """
+    if not hasattr(obj, '__dict__'):
+        raise ValueError("Input is not an instance of a class.")
+
+    # Initialize an empty dictionary to store the serializable attributes
+    json_dict = {}
+
+    # Loop through the object's attributes
+    for attr, value in obj.__dict__.items():
+        # Check if the attribute is serializable (list, dict, str, int, or bool)
+        if isinstance(value, (list, dict, str, int, bool)):
+            # Add the attribute and its value to the dictionary
+            json_dict[attr] = value
+
+    return json_dict
