1ad00638e4aa14b03050557beeac72ab369d7337
Nathan Machoka <natemachoka@gmail.com>
2024-06-13 05:27:56 -0700

    4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,5 @@
 --create first table with name and id
 
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256))
+CREATE TABLE IF NOT EXISTS first_table (
+		id INT,
+		name VARCHAR(256));
