c23a139e261d617fdd416e0f7c02b2e8252158c0
Nathan Machoka <natemachoka@gmail.com>
2024-06-14 08:06:06 -0700

    changed 4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,4 @@
 -- Create first table with specific schema
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (
+		id INT,
+		name VARCHAR(256));
