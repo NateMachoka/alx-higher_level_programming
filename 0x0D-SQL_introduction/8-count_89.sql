20703af90dca82122a2d791e65dd8517eb5946ea
Nathan Machoka <natemachoka@gmail.com>
2024-06-14 08:12:18 -0700

    8-count_89.sql

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
---count the number of records with id = 89 in first_table
+-- Count the number of records with id = 89 in first_table
 SELECT COUNT(*) AS record_count FROM first_table WHERE id = 89;
