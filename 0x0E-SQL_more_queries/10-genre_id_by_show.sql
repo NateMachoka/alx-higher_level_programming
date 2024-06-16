549de5f8e0f450013725d99d0c847575a3346a27
Nathan Machoka <natemachoka@gmail.com>
2024-06-16 02:54:29 -0700

    fixed 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- Lists all tv shows in a database
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_shows.genre ON tv_shows.id=tv_shows_genre.show_id
+JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
