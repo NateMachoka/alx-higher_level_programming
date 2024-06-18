dc4b35d5d69024c7af7ab6ebb71ca483d017ddff
NateMachoka <natemachoka@gmail.com>
2024-06-18 10:06:55 -0700

    10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+// Function to compute factorial recursively
+function factorial(n) {
+  if (isNaN(n) || n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+// Retrieve the first argument and convert it to an integer
+const arg = parseInt(process.argv[2]);
+
+console.log(factorial(arg));
