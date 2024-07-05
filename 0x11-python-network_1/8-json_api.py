fa658d45404e559fbcc299fd6b2523877eda70e7
Nathan Machoka <natemachoka@gmail.com>
2024-07-05 08:18:47 -0700

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-A script that sends a POST request to a specified URL (http://0.0.0.0:5000/search_user)
+A script that sends a POST request to a specified URL
 with a parameter (q) based on user input.
 """
 import requests
@@ -9,7 +9,7 @@ import sys
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    
+
     if len(sys.argv) < 2:
         q = ""
     else:
@@ -20,12 +20,11 @@ if __name__ == "__main__":
         json_response = response.json()
 
         if json_response:
-            print("[{}] {}".format(json_response.get('id'), json_response.get('name')))
+            print("[{}] {}".format
+                  (json_response.get('id'), json_response.get('name')))
         else:
             print("No result")
-    
     except ValueError:
         print("Not a valid JSON")
-    
     except Exception as e:
         print("Error:", e)
