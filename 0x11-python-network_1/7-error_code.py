2d47dbc4b271d3cace6252628d004437ebbf035c
Nathan Machoka <natemachoka@gmail.com>
2024-07-05 07:56:12 -0700

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,13 +9,27 @@ import requests
 import sys
 
 
-url = sys.argv[1]
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./script_name.py <URL>")
+        sys.exit(1)
+    
+    url = sys.argv[1]
 
-try:
-    response = requests.get(url)
-    response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
-    print(response.text)
-except requests.exceptions.HTTPError as e:
-    print(f"Error code: {e.response.status_code}")
-except requests.exceptions.RequestException as e:
-    print(f"Error: {e}")
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
+
+        # Check if status code is 400 or higher
+        if response.status_code >= 400:
+            print(f"Error code: {response.status_code}")
+        else:
+            print(response.text)
+    
+    except requests.exceptions.HTTPError as e:
+        # Error response handling
+        print(f"Error code: {e.response.status_code}")
+    
+    except requests.exceptions.RequestException as e:
+        # Connection error handling
+        print(f"Error: {e}")
