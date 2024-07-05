fbb33d5198bfdd613bedbc51460126241b575150
Nathan Machoka <natemachoka@gmail.com>
2024-07-05 06:50:31 -0700

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,11 +9,14 @@ import urllib.error
 import sys
 
 
-url = sys.argv[1]
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-try:
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        print(body.decode('utf-8'))
-except urllib.error.HTTPError as e:
-    print("Error code: {}".format(e.code))
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read()
+            print(body.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
+    except urllib.error.URLError as e:
+        print("Error: {}".format(e.reason))
