1c537ca883778248756aab21dfd33cfa38be0ac1
NateMachoka <natemachoka@gmail.com>
2024-07-30 06:14:18 -0700

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,6 +3,8 @@
 
 const request = require('request');
 const apiUrl = process.argv[2];
+// Define the ID for movie character 'Wedge Antilles'
+const AntillesId = 'https://swapi-api.alx-tools.com/api/people/18/';
 
 request(apiUrl, (error, response, body) => {
   if (error) {
@@ -15,7 +17,7 @@ request(apiUrl, (error, response, body) => {
 
   // Iterate over each movie in the result
   data.results.forEach(movie => {
-    if (movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+    if (movie.characters.includes(AntillesId)) {
       count++;
     }
   });
