83f5eeba696616d0b9bc2e4948dea2b35a34708b
Nathan Machoka <natemachoka@gmail.com>
2024-08-12 09:32:44 -0700

    8-script.js

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $.getJSON('https://swapi-api.alx-tools.com/api/films/?format=json', function(data) {
+        const movies = data.results;
+        const $listMovies = $('#list_movies');
+
+        movies.forEach(function(movie) {
+            $listMovies.append(`<li>${movie.title}</li>`);
+        });
+    });
+});
